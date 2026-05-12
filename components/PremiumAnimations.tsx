af1f73589af3f2fd5ab893bd262411fc04c8fc95
john <john@gmail.com>
2026-05-12 09:41:04 +0100

    DFGFDS

--- components/PremiumAnimations.tsx
+++ components/PremiumAnimations.tsx
@@ -46,14 +46,12 @@ export const AnimatedButton: React.FC<AnimatedButtonProps> = ({
     lg: 'px-8 py-4 text-lg',
   };
 
-  const { onDrag, onDragStart, onDragEnd, ...safeProps } = props;
-
   return (
     <motion.button
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.98 }}
       className={`${baseClasses} ${variants[variant]} ${sizes[size]} ${className}`}
-      {...safeProps}
+      {...props}
     >
       {children}
     </motion.button>
